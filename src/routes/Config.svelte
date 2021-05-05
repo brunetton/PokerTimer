cf2954a4a1bec1f2fadccf75bbb598badd935daf
Bruno Duyé <brunetton@brunetton.org>
2021-05-06 00:52:23 +0200

    Add config

---
+++ src/routes/Config.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { initialSmallBlind, roundDuration } from "../store"
+</script>
+
+<div>
+	<p>Initial SB: <input type=number bind:value={$initialSmallBlind} min=1 max=500><input type=range bind:value={$initialSmallBlind} min=1 max=500></p>
+	<p>Round duration (in seconds): <input type=number bind:value={$roundDuration} min=1 max=500><input type=range bind:value={$roundDuration} min=1 max=500></p>
+</div>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import Counter from "./Counter.svelte"
+    import Config from "./Config.svelte"
 </script>
+
+<Config />
+
+<div style="height: 2em"></div>
 
 <Counter />
